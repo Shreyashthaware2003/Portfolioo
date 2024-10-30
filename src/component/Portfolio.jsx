97d70c3acfc65164969339688d33070e55d422a3
Shreyashthaware2003 <shreyashthaware284@gmail.com>
2024-10-30 13:23:36 +0530

    added pagination and fixed it

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -4,13 +4,15 @@ import { BiMenuAltRight } from "react-icons/bi";
 import { IoClose } from "react-icons/io5";
 import { FiGithub } from "react-icons/fi";
 import { IoLink } from "react-icons/io5";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 function Portfolio() {
     const [color, setColor] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
     const itemsPerPage = 3;
-    
+
 
     const project = [
         {
@@ -22,38 +24,38 @@ function Portfolio() {
         },
         {
             imgSrc: '/itech.jpg',
-            imgAlt: 'Spotify - Clone',
+            imgAlt: 'iTech -  Roots',
             git: '#',
-            project: 'https://spotify-clonee1.vercel.app/',
-            title: 'Spotify - Clone'
+            project: 'https://itech-roots.vercel.app/',
+            title: 'iTech - Roots'
         },
         {
             imgSrc: '/doctor.png',
-            imgAlt: 'Spotify - Clone',
+            imgAlt: 'CarePoint',
             git: '#',
-            project: 'https://spotify-clonee1.vercel.app/',
-            title: 'Spotify - Clone'
+            project: 'https://carepointt.vercel.app/',
+            title: 'CarePoint'
         },
         {
             imgSrc: '/store.png',
-            imgAlt: 'Spotify - Clone',
+            imgAlt: 'Quantum - Store',
             git: '#',
-            project: 'https://spotify-clonee1.vercel.app/',
-            title: 'Spotify - Clone'
+            project: 'https://quantum-storee1.vercel.app/',
+            title: 'Quantum - Store'
         },
         {
             imgSrc: '/text.jpg',
-            imgAlt: 'Spotify - Clone',
+            imgAlt: 'Text - Convertor',
             git: '#',
-            project: 'https://spotify-clonee1.vercel.app/',
-            title: 'Spotify - Clone'
+            project: 'https://text-convertor1.vercel.app/',
+            title: 'Text - Convertor'
         },
         {
             imgSrc: '/itask.png',
-            imgAlt: 'Spotify - Clone',
+            imgAlt: 'iTask - ToDo List',
             git: '#',
-            project: 'https://spotify-clonee1.vercel.app/',
-            title: 'Spotify - Clone'
+            project: 'https://todo-list-psi-cyan.vercel.app/',
+            title: 'iTask - ToDo List'
         },
 
     ];
@@ -131,29 +133,43 @@ function Portfolio() {
                     </nav>
 
                     {/* Main Content */}
-                    <div className='py-16'>
-                        <h1 className={`${color ? 'text-white' : 'text-black'} text-4xl font-bold text-center`}>My Portfolio</h1>
-                        <div className={`grid md:grid-cols-2 gap-8 justify-center items-center tracking-wide py-16 ${color ? 'text-white' : 'text-black'}`}>
-                            <div className='text-sm md:text-base font-semibold px-6 md:py-24 md:px-24'>
-                                <h2 className='text-2xl font-bold py-2'>Hello, I'm Shreyash Thaware</h2>
-                                <p className='py-2'>Dedicated to creating impactful tech projects!</p>
+                    <div className='py-24 flex flex-col  items-center'>
+                        {/* <h1 className={`${color ? 'text-white' : 'text-black'} text-4xl font-bold text-center`}>My Portfolio</h1> */}
+                        <div className={`grid md:grid-cols-2 md:w-[900px]  md:gap-0 justify-center items-center tracking-wide py-4 md:py-20 ${color ? 'text-white' : 'text-black'}`}>
+                            <div className=' text-sm md:text-base font-semibold px-6 md:py-24 md:px-24 order-2 md:order-1 flex flex-col md:text-start md:w-[600px]'>
+                                <h2 className='text-2xl font-bold py-2 flex justify-center md:block'>Hello,<br className='md:hidden' />  I'm Shreyash Thaware</h2>
+                                <p className='px-5 md:px-0'>Dedicated to creating impactful tech projects!</p>
+                                <div className='flex md:justify-start gap-4 py-6 px-5 md:px-0'>
+                                    <a href="" className={`w-8 h-8 rounded-full shadow-lg bg-gray-100 text-lg hover:bg-blue-600 hover:text-white hover:scale-125 duration-300 flex justify-center items-center ${color ? 'text-black border border-gray-400' : ''} `}><FaLinkedinIn /></a>
+                                    <a href="" className={`w-8 h-8 rounded-full shadow-lg bg-gray-100 text-lg hover:bg-pink-600 hover:text-white hover:scale-125 duration-300 flex justify-center items-center ${color ? 'text-black border border-gray-400' : ''}`}><FaInstagram className='font-bold' /></a>
+                                    <a href="" className={`w-8 h-8 rounded-full shadow-lg bg-gray-100 text-lg hover:bg-black hover:text-white hover:scale-125 duration-300 flex justify-center items-center ${color ? 'text-black border border-gray-400' : ''}`}><FiGithub /></a>
+                                </div>
+                                <a href="/Resume1.pdf" download="Shreyash_Thaware_Resume.pdf" className='px-5 md:px-0'>
+                                    <button className={`bg-blue-600  font-bold text-xl w-[126px] h-[46px] rounded-2xl shadow-md shadow-gray-500 hover:scale-105 duration-200 ${color ? 'bg-[#67fd67] text-black' : 'text-white'} `}>
+                                        Resume
+                                    </button>
+                                </a>
                             </div>
-                            <div className='flex items-center justify-center'>
+                            <div className='flex items-center justify-center order-1 md:order-2'>
                                 <img src="/hero.png" alt="Hero" className='w-56 h-auto md:w-auto' />
                             </div>
                         </div>
 
                         {/* Projects Section with Pagination */}
                         <div className='text-center py-8'>
-                            <h2 className='text-2xl font-bold uppercase'>Projects</h2>
-                            <div className='flex flex-wrap justify-center gap-8 py-4'>
+                            <div className='flex justify-center items-center gap-6 py-8  md:px-10'>
+                                <div className='line-before h-[1.5px] w-full bg-slate-300 '></div>
+                                <h2 className={`text-2xl font-bold uppercase ${color ? 'text-white' : ''}`}>Projects</h2>
+                                <div className='line-after h-[1.5px] w-full bg-slate-300 '></div>
+                            </div>
+                            <div className='flex flex-wrap justify-center gap-4 py-4'>
                                 {currentProjects.map((item, index) => (
-                                    <a href={item.project} target='_blank' key={index} className={`flex flex-col border-2 border-black rounded-md ${color ? 'text-white' : 'text-black'}`}>
-                                        <img src={item.imgSrc} alt={item.imgAlt} className='w-80 object-cover' />
-                                        <div className='flex justify-between items-center px-4 py-2 capitalize font-semibold'>
+                                    <a href={item.project} target='_blank' key={index} className={`flex flex-col mx-2 md:mx-0 border-2 border-black rounded-md ${color ? 'text-white border-gray-400' : 'text-black'}`}>
+                                        <img src={item.imgSrc} alt={item.imgAlt} className='w-96 md:h-[185px] object-cover rounded-t-md' />
+                                        <div className='flex justify-between items-center px-4 py-2  font-semibold'>
                                             <span className='flex gap-2'>
-                                                <a href={item.git}><FiGithub className='rounded-full w-8 h-8 py-2 shadow-lg bg-white' /></a>
-                                                <a href={item.project}><IoLink className='rounded-full w-8 h-8 py-2 shadow-lg bg-white' /></a>
+                                                <a href={item.git}><FiGithub className={`rounded-full hover:scale-110 duration-100 w-8 h-8 py-2 shadow-lg bg-white ${color ? 'text-black' : ''}`} /></a>
+                                                <a href={item.project}><IoLink className={`rounded-full hover:scale-110 duration-100 w-8 h-8 py-2 shadow-lg bg-white ${color ? 'text-black' : ''}`} /></a>
                                             </span>
                                             {item.title}
                                         </div>
@@ -161,10 +177,31 @@ function Portfolio() {
                                 ))}
                             </div>
                             <div className='flex justify-center mt-4 gap-2'>
-                                <button onClick={goToPreviousPage} disabled={currentPage === 1} className={`cursor-pointer  w-4 h-4 bg-gray-300 rounded-full ${color ? 'bg-gray-300' : 'bg-blue-500 shadow-lg border-2 border-black'} `}></button>
-                                {/* <span className='text-sm'>{currentPage} / {totalPages}</span> */}
-                                <button onClick={goToNextPage} disabled={currentPage === totalPages} className={`cursor-pointer  w-4 h-4 bg-gray-300 rounded-full ${color ? 'bg-red-500' : 'bg-black shadow-lg border-2 border-black'} `}></button>
+                                {Array.from({ length: totalPages }).map((_, index) => (
+                                    <button
+                                        key={index}
+                                        onClick={() => setCurrentPage(index + 1)}
+                                        className={`cursor-pointer w-4 h-4 rounded-full shadow-lg border-2 border-black ${currentPage === index + 1 ? (color ? 'bg-[#67fd67]' : 'bg-blue-500') : 'bg-gray-300'}`}></button>
+                                ))}
                             </div>
+                        </div>
+                        {/* Mobile Toggle Switch at the Bottom */}
+                        <div className='md:hidden fixed bottom-10 right-10 flex justify-center py-2'>
+                            <label className="relative inline-flex items-center cursor-pointer">
+                                <input
+                                    type="checkbox"
+                                    className="sr-only peer"
+                                    checked={color}
+                                    onChange={() => setColor(!color)}
+                                />
+                                <div className={`w-8 h-8 bg-gray-200 rounded-full peer-focus:outline-none shadow-lg peer-checked:bg-black transition-all flex items-center justify-center px-1 border-2 ${color ? 'border-gray-700' : ''}`}>
+                                    {color ? (
+                                        <FaSun className="text-yellow-500 text-xl" />
+                                    ) : (
+                                        <FaMoon className="text-black text-xl" />
+                                    )}
+                                </div>
+                            </label>
                         </div>
                     </div>
                 </div>
